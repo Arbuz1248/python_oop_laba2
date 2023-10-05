e735b229eac5573b8a118fde806f4cbafc40129f
IGOR 2000 <aiblaze@yandex.ru>
2023-10-05 16:05:33 +0300

    quest 3

---
+++ 3_quest.py
@@ -0,0 +1,13 @@
+class Employee:
+ gigast = None
+ name = None
+ age = None
+ salary = None
+timofey = Employee()
+timofey.name = "Kuznetsow"
+timofey.age = 18
+timofey.salary = 1150
+
+
+
+print(f"Данные сотрудника: имя: {timofey.name}, возраст(неточный): {timofey.age}, стипендия: {timofey.salary}. ")
